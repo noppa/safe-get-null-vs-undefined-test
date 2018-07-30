aa24390579eacea0b35577d9b37eec5225268a7c
Oskari Noppa <the.nobba@gmail.com>
2018-07-30 23:05:01 +0300

    Add docs & more libs

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const tbody = document.getElementById('results');
 
 function r(
   libname,
-  test
+  test,
+  docsUrl,
 ) {
   // Sanity check: make sure the function implementation actually works:
   console.assert(test({ a: { b: 42, }}) === 42, `Test for ${libname} failed!`)
@@ -14,7 +15,7 @@ function r(
 
   const testScript = test.toString().replace('obj => ', '');
   const html = `<tr>
-    <td>${libname}</td>
+    <td><a href="${docsUrl}">${libname}</a></td>
     <td><code>${result}</code></td>
     <td><code>${testScript}</code></td>  
   </tr>`;
@@ -26,25 +27,36 @@ document.getElementById('loading').remove();
 
 r(
   'underscore',
-  obj => underscore.property(['a', 'b'])(obj)
+  obj => underscore.property(['a', 'b'])(obj),
+  'https://underscorejs.org/#property'
 )
 
 r(
   'lodash',
-  obj => lodash.get(obj, ['a', 'b'])
+  obj => lodash.get(obj, 'a.b'),
+  'https://lodash.com/docs/4.17.10#get'
 )
 
 r(
   'ramda',
-  obj => R.path(['a', 'b'])(obj)
+  obj => R.path(['a', 'b'])(obj),
+  'https://ramdajs.com/docs/#path'
 )
 
 r(
   'closure',
-  obj => goog.object.getValueByKeys(obj, 'a', 'b')
+  obj => goog.object.getValueByKeys(obj, 'a', 'b'),
+  'https://google.github.io/closure-library/api/goog.object.html#getValueByKeys'
 )
 
 r(
   'idx',
-  obj => idx(obj, _ => _.a.b)
+  obj => idx(obj, _ => _.a.b),
+  'https://github.com/facebookincubator/idx'
+)
+
+r(
+  'Ember',
+  obj => Ember.get(obj, 'a.b'),
+  'https://www.emberjs.com/api/ember/3.3/functions/@ember%2Fobject/get'
 )
